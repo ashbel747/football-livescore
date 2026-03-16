e6107eea217b41c7aeb08d34b196118c8b6a94b4
Ashbel King’ori <kashbel747@gmail.com>
2026-03-16 21:38:09 +0300

    Add Initial Frontend Structure

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { rawBody: true });
-
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
