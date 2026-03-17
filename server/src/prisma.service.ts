658711e5b2ab6d58c14b7c869221cdc3be234cff
Ashbel King’ori <kashbel747@gmail.com>
2026-03-17 18:26:23 +0300

    Resolve prisma constructor error

--- server/src/prisma.service.ts
+++ server/src/prisma.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor(private config: ConfigService) {
-    super({
-      log: ['error'],
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
     });
+    super({ adapter });
   }
 
   async onModuleInit() {
